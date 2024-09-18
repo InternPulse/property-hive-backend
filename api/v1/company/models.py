e8f521687497ccf1aa928d6efd37e3b07b1499c4
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-19 00:27:54 +0100

    Refactor: Add RealEstateCompany model with custom URL field

--- api/v1/company/models.py
+++ api/v1/company/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from common.models import User
 
-# Create your models here.
+
+class RealEstateCompany(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="company")
+    custom_url = models.CharField(max_length=255, blank=True, null=True)
