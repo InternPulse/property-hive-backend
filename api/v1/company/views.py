6365af5889f6ff82954a3214e6fc99cb615c9c12
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-18 21:53:19 +0100

    Refactor: Update company views to generate custom URL
    This commit refactors the company views in the  file. It adds a new view function  that generates a custom URL for a company based on its name. The custom URL is generated using the  function from Django's  module. If the company already has a custom URL, the function does not generate a new one. The generated custom URL is saved in the  field of the company model and returned in the JSON response.
    
    #chore

--- api/v1/company/views.py
+++ api/v1/company/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.utils.text import slugify
+from django.shortcuts import get_object_or_404
+from .models import Company
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(["POST"])
+def generate_custom_url(request):
+    company_id = request.data.get("company_id")
+    company = get_object_or_404(Company, id=company_id)
+
+    if not company.custom_url:
+        slug = slugify(company.name)
+        company.custom_url = f"{slug}.propertyhive.com"
+        company.save()
+
+    return JsonResponse(
+        {
+            "statusCode": 200,
+            "message": "Custom URL generated successfully",
+            "data": {"customUrl": company.custom_url},
+        }
+    )
