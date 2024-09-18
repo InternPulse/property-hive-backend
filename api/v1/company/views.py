c41564516f56fd49f1dfab5e6de54b9a08bb04f3
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-19 00:28:53 +0100

    Refactor: Update company URLs to use viewset for custom URL generation

--- api/v1/company/views.py
+++ api/v1/company/views.py
@@ -1,24 +1,13 @@
 from django.http import JsonResponse
 from django.utils.text import slugify
 from django.shortcuts import get_object_or_404
-from .models import Company
-from rest_framework.decorators import api_view
+from .models import RealEstateCompany
+from rest_framework import viewsets
 
 
-@api_view(["POST"])
-def generate_custom_url(request):
-    company_id = request.data.get("company_id")
-    company = get_object_or_404(Company, id=company_id)
+class RealEstateCompanyViewSet(viewsets.ViewSet):
+    serializer_class = RealEstateCompany
+    queryset = RealEstateCompany.objects.all()
 
-    if not company.custom_url:
-        slug = slugify(company.name)
-        company.custom_url = f"{slug}.propertyhive.com"
-        company.save()
-
-    return JsonResponse(
-        {
-            "status_code": 200,
-            "message": "Custom URL generated successfully",
-            "data": {"custom_url": company.custom_url},
-        }
-    )
+    def create(self, serializer):
+        serializer.save()
