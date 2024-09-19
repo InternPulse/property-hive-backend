307c901ebe77c7e7abec12cec167174384417bbe
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-19 13:14:03 +0100

    Refactor: Update property_hive/urls.py

--- property_hive/urls.py
+++ property_hive/urls.py
@@ -7,16 +7,17 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from rest_framework.routers import DefaultRouter
+
 # from api.v1.custom_auth.views import UserViewset
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # path("api/v1/", include("api.v1.company.urls")),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/v1/", include("api.v1.company.urls")),
     # path("api/v1/", include("api.v1.custom_auth.urls")),
     # path("api/v1/", include("api.v1.transaction.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # router = DefaultRouter()
 # router.register('user',UserViewset ,basename='user')
 
