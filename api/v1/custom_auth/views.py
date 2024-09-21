d4e0038f3540f04cb7c1379d88ca6573790067ca
abdulhamidbabawale <hamidbabawale@gmail.com>
2024-09-21 06:35:20 +0100

    register updates

--- api/v1/custom_auth/views.py
+++ api/v1/custom_auth/views.py
@@ -6,13 +6,15 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
 from api.v1.common.models import User
+from rest_framework.permissions import AllowAny
 class UserViewset(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
 class RegisterCompany(APIView):
-    def Post(self,request):
+    permission_classes = [AllowAny]
+    def post(self,request):
         serializer=UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
