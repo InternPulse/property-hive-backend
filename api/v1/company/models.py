e026d58dfb21f16ca98d226a1ba296876846b1b8
Samilincoln <agboolaola150@gmail.com>
2024-09-23 21:58:34 +0100

    added views and model for company profile with logout view(token based)

--- api/v1/company/models.py
+++ api/v1/company/models.py
@@ -1,7 +1,12 @@
 from django.db import models
-from ..common.models import User
+from ..common.models import User, Profile
 
 
 class RealEstateCompany(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="company")
     custom_url = models.CharField(max_length=255, blank=True, null=True)
+
+class CompanyProfile(models.Model):
+    phone_number = models.OneToOneField(User, on_delete=models.CASCADE, related_name="company_phone_number")
+    company_address = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name="company_address")
+    company_logo = logo = models.ImageField(upload_to='media/', verbose_name='image')
--- api/v1/company/serializers.py
+++ api/v1/company/serializers.py
@@ -1,6 +1,6 @@
 from ..common.models import User
 from rest_framework import serializers
-from .models import RealEstateCompany
+from .models import RealEstateCompany, CompanyProfile
 from django.utils.text import slugify
 
 
@@ -8,3 +8,12 @@ class RealEstateSerialize(serializers.ModelSerializer):
     class Meta:
         model = RealEstateCompany
         fields = ["user", "custom_url"]
+
+
+
+
+class CompanyProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompanyProfile
+        fields = ['phone_number', 'company_address', 'company_logo']
+
