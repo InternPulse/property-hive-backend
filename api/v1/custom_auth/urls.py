37b69c14ec9f95851098fd6cd099f068ad23f3a8
abdulhamidbabawale <135306198+abdulhamidbabawale@users.noreply.github.com>
2024-09-23 20:08:21 +0100

    Revert "Forgot password endpoint & Reset password endpoint"

--- api/v1/custom_auth/urls.py
+++ api/v1/custom_auth/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, include
-from .views import UserProfileView, RegisterCompany, ForgotPasswordView, ResetPasswordView
+from .views import UserProfileView, RegisterCompany
 
 urlpatterns = [
     path("profile/", UserProfileView.as_view(), name="user_profile"),
     path('register/', RegisterCompany.as_view(), name="register_company"),
-    path('forgot-password', ForgotPasswordView.as_view(), name='forgot-password'),
-    path('reset-password', ResetPasswordView.as_view(), name='reset-password'),
 ]
