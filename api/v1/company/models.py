4a60e684c5ac752cfb04b60d64498d5828abc548
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-25 23:48:09 +0100

    Refactor company models by removing commented out code

--- api/v1/company/models.py
+++ api/v1/company/models.py
@@ -5,8 +5,3 @@ from ..common.models import User, Profile
 class RealEstateCompany(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="company")
     custom_url = models.CharField(max_length=255, blank=True, null=True)
-
-# class CompanyProfile(models.Model):
-#     phone_number = models.OneToOneField(User, on_delete=models.CASCADE, related_name="company-phone-number")
-#     company_address = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name="company-address")
-#     company_logo =  models.OneToOneField(Profile, on_delete=models.CASCADE, related_name="company-logo")
