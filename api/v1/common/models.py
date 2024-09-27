f7420fedab461f7a2ba6f90b24771856a1013005
abdulhamidbabawale <hamidbabawale@gmail.com>
2024-09-27 03:02:45 +0100

    business_name null=true

--- api/v1/common/models.py
+++ api/v1/common/models.py
@@ -35,7 +35,7 @@ class User(AbstractBaseUser,PermissionsMixin):
     email= models.EmailField(blank=True, default='', unique=True)
     fname = models.CharField(max_length=255, blank=True)
     lname = models.CharField(max_length=255, blank=True)
-    business_name = models.CharField(max_length=255, blank=True ,unique=True)
+    business_name = models.CharField(max_length=255, blank=True ,unique=True ,null=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True, unique=True)
     profile_picture = models.ImageField(upload_to='user_avatars/', blank=True, null=True)
     custom_url = models.URLField(max_length=500, blank=True, null=True)
