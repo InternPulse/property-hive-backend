b6aa6e27076221f13d824ad83cc322642941bb9e
abdulhamidbabawale <135306198+abdulhamidbabawale@users.noreply.github.com>
2024-10-02 17:47:41 +0100

    Update settings.py

--- property_hive/settings.py
+++ property_hive/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = "property_hive.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {"default": dj_database_url.parse(os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"))}
+DATABASES = {"default": dj_database_url.parse(os.getenv("DATABASE_URL"))}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
