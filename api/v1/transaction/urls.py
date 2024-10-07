d6c2dd7bc066721d3aa02eee11e1eb82d9b15584
Samilincoln <agboolaola150@gmail.com>
2024-10-07 10:25:58 +0100

    created earning view

--- api/v1/transaction/urls.py
+++ api/v1/transaction/urls.py
@@ -1,11 +1,12 @@
 # api/v1/transaction/urls.py
 
 from django.urls import path
-from .views import TransactionListView, TransactionDetailView,PropertyInvoiceView
+from .views import TransactionListView, TransactionDetailView,PropertyInvoiceView, EarningDetailsView
 
 urlpatterns = [
     path('transactions/', TransactionListView.as_view(), name='transaction-list'),
     path('transactions/<int:transaction_id>/', TransactionDetailView.as_view(), name='transaction-detail'),
     path('invoice/', PropertyInvoiceView.as_view(), name='invoice'),
     path('invoice/<int:invoice_id>/', PropertyInvoiceView.as_view(), name='invoice'),
+    path('earnings/', EarningDetailsView.as_view(), name='earning-details'),
 ]
